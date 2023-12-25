dcc7859044791de26958a14779a2d2bbb3068ccf
nedoratic <nedoratic@gmail.com>
2023-12-25 22:28:59 +0100

    app.jsx - static content layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 function App() {
-	return "Setup test!";
+	return (
+		<div className="app">
+			<h1 className="app__title">To Do App</h1>
+			<form className="app__form">
+				<label className="app__form__label" htmlFor="app__form__input">
+					New Item
+				</label>
+				<input className="app__form__input" id="app__form__input" type="text" />
+			</form>
+			<button className="app__btn btn btn-add">Add Item</button>
+			<h2 className="app__header">To Do List</h2>
+			<ul className="app__list">
+				<li className="app__list__item">
+					<label className="app__list__item__label" htmlFor="">
+						<input className="app__list__item__checkbox" type="checkbox" />
+						Item 1
+					</label>
+					<button className="app__list__item__btn btn btn-delete">Delete</button>
+				</li>
+				<li className="app__list__item">
+					<label className="app__list__item__label" htmlFor="">
+						<input className="app__list__item__checkbox" type="checkbox" />
+						Item 2
+					</label>
+					<button className="app__list__item__btn btn btn-delete">Delete</button>
+				</li>
+				<li className="app__list__item">
+					<label className="app__list__item__label" htmlFor="">
+						<input className="app__list__item__checkbox" type="checkbox" />
+						Item 3
+					</label>
+					<button className="app__list__item__btn btn btn-delete">Delete</button>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
